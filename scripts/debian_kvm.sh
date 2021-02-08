ac1e98dd470ecc65fb2544e56c43dd10fac7f65e
cmejia <cesarmejia555@yahoo.com.ar>
2021-02-08 20:18:09 -0300

    Added kvm

---
+++ scripts/debian_kvm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+if [ $(whoami) != "root" ]; then
+	echo "Run this script '$0' as root"
+	exit 1
+fi
+
+echo "Installing KVM..."
+
+echo "Checking if your processor support hardaware virtualization"
+virt=$(grep -E --color '(vmx|svm)' /proc/cpuinfo | wc -l)
+if [ $virt -eq 0 ]; then
+	echo "Hardware virtualization is not enabled"
+	exit 1
+fi
+
+apt-get install --no-install-recommends --yes qemu-system qemu-utils libvirt-daemon-system libvirt-clients virtinst
+echo "Installing KVM...DONE"
+
+exit 0
+
