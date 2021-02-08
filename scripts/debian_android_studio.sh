3b50a7a8726ed2dd553d0088177d123093185a55
cmejia <cesarmejia555@yahoo.com.ar>
2021-02-08 19:58:33 -0300

    Fix paths

--- scripts/debian_android_studio.sh
+++ scripts/debian_android_studio.sh
@@ -12,32 +12,32 @@ fi
 
 echo "Installing Android Studio..."
 
-echo "Download Android Studio from https://developer.android.com/studio#downloads in ~/Download folder"
+echo "Download Android Studio from 'https://developer.android.com/studio' in ~/Download folder"
 read -p "Press [ENTER] after to download app"
 
-cd ~/Downloads
+cd /home/$USERNAME/Downloads
 # Check if app exists
 [[ ! -e $(ls | grep android-studio-*.tar.gz) ]] && echo "You have not downloaded Android Studio yet" && exit 1
 
 mkdir -p $ANDROID_INSTALL_PATH
 tar -xzvf android-studio-ide-*-linux.tar.gz -C $ANDROID_INSTALL_PATH --strip-components=1
-
 chown -R $USERNAME:$USERNAME $ANDROID_INSTALL_PATH
+rm android-studio-*.tar.gz
 
-
+cd /home/$USERNAME
 ANDROID_ENV_TITLE="# Android environment variables"
-
-if [ $(grep -x "$ANDROID_ENV_TITLE" ~/.bashrc | wc -l) -eq 0 ]; then
-	echo $ANDROID_ENV_TITLE >> ~/.bashrc
-	echo '' >> ~/.bashrc
-	echo '' >> ~/.bashrc
-	echo '' >> ~/.bashrc
-	echo '' >> ~/.bashrc
+if [ $(grep -x "$ANDROID_ENV_TITLE" .bashrc | wc -l) -eq 0 ]; then
+	echo $ANDROID_ENV_TITLE >> .bashrc
+	echo 'export ANDROID_HOME=$HOME/Android/Sdk' >> .bashrc
+	echo 'export PATH=$PATH:$ANDROID_HOME/platform-tools' >> .bashrc
+	echo 'export PATH=$PATH:$ANDROID_HOME/tools' >> .bashrc
+	echo '' >> .bashrc
 fi
 
-echo "Logout as root and run $ANDROID_INSTALL_PATH/bin/studio.sh script"
+echo "Logout as root and to run '$ANDROID_INSTALL_PATH/bin/studio.sh' script"
 read -p "Press [ENTER] to continue."
 
 echo "Installing Android Studio...DONE"
 
 exit 0
+
